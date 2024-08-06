4ec81d45e11f000770cb1ae5dd1e2f1634017819
serim <liouxe@naver.com>
2024-08-06 17:49:21 +0900

    20240806 2nd

--- src/pracatice/bj_1592_영식이와친구들.java
+++ src/pracatice/bj_1592_영식이와친구들.java
@@ -3,7 +3,7 @@ package pracatice;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class practice {
+public class bj_1592_영식이와친구들 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -12,19 +12,30 @@ public class practice {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int l = sc.nextInt();
-	
-		int[] arr = new int[n];
 
+		int[] arr = new int[n+1];
 		int idx = 1;
-		int left_idx = (idx + l) % (n-1);
-		int right_idx = (idx - l) % (n-1);
-
-		for (int i = 0; i < n ; i++) {
+		int cnt = 0;
+		
+		for (int i = 1; i < n+1; i++) {
 			if (arr[i] == m) {
 				break;
-			} else if (arr[idx])
 				
+			} else if (arr[idx] % 2 == 0) {
+				idx = idx - l;
+				cnt++;
+				if (idx < 0)
+					idx = idx + n;
+				System.out.println(idx);
+			} else if (arr[idx] % 2 != 0) {
+				idx = (idx + l) % (n - 1);
+				cnt++;
+				System.out.println(idx);
+			}
+
 		}
+		
+		System.out.println(cnt);
 
 	}
 }
